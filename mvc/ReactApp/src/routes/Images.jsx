c6c35a5336b7c9e9fe4c3b90ecdfa93ebaff54c6
Mathias Angelin <mathias.angelin@gmail.com>
2022-04-11 14:37:07 +0200

    hr

--- mvc/ReactApp/src/routes/Images.jsx
+++ mvc/ReactApp/src/routes/Images.jsx
@@ -1,19 +1,59 @@
+import React from 'react';
+import axios from 'axios';
+
 export default function Images() {
-  return (
-    <div className="images">
-      <h1>Bilder</h1>
-      <button className="button" id="createimagebutton">Skapa</button>
-     
-      <div id="imgRow1">
-      <label >
+
+    const [selectedFile, setSelectedFile] = React.useState(null);
+  
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      const formData = new FormData();
+      formData.append("selectedFile", selectedFile);
+      try {
+         axios({
+          method: "post",
+          url: "https://localhost:7208/api/images",
+          data: formData,
+          headers: { "Content-Type": "multipart/form-data" },
+        }).then(addmessage);
+      } catch(error) {
+        console.log(error)
+      }
+    }
+  
+    function addmessage() {
+
+      let ptag = document.createElement("p");
+      let successMessage = document.createTextNode("Uppladdning lyckades");
+      ptag.appendChild(successMessage);
+      ptag.setAttribute("id", "displayMessage")
+      let element = document.getElementById("Messageshow");
+      element.appendChild(ptag);
+      setTimeout(hideElement, 3000)
+
+    }
+
+  function hideElement() {
+    let ptag = document.getElementById("displayMessage")
+    ptag.style.display = 'none'
+  }
+    const handleFileSelect = (event) => {
+      setSelectedFile(event.target.files[0])
+    }
+  
+    return (
+      <div className="images">
+          <h1>Bilder</h1>
+          <button className="button" id="createimagebutton">Skapa</button>
+          <div id="Messageshow"></div>
+        <form onSubmit={handleSubmit}>
+        <label for="chosefile">
         Label Name 
-        <input type="file" name="name" />
-      </label>
-      <button className="button" id="searchImg">Bläddra</button>
+        <input type="file" name="chosefile" onChange={handleFileSelect}/>
+         </label>
+        <input className="button" type="submit" id="uploadImg" value="Ladda upp" />
+      </form>
       </div>
+    )
+  };
 
-      <button className="button" id="uploadImg">Ladda upp</button>
-
-    </div>
-  );
-}
